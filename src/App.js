0964d7da3ac5aa35a946a5981256b72f5b4f0a9b
samara rebeca <samara.rebeca@escolar.ifrn.edu.br>
2023-07-19 11:24:10 -0300

    SiteDear

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Cabecalho from './componentes/Cabecalho'
+import Conteudo,{OlaMundo} from './componentes/Conteudo' 
+import Rodape from './componentes/Rodape'
 
+/*
+Arquivo App JSX
+Componentem funcional = Porque ele é uma função.
+*/
 function App() {
+
+  OlaMundo();
+/* todo componente de retornar  um html entre parenteses*/
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    /* O react substitui className por class porque class é palavra reservada pro JS */
+    <>
+    <img src= 'logo.png' alt= 'logotipo'></img>
+    {/* Cabecalho tem um propriedade chamada titulo*/}
+    <Cabecalho  logo ="logo192.png" titulo = "Fav.io" subtitulo="The bessssttttt!!"/>
+    <Conteudo/> 
+    <Rodape/>
+    </>
   );
 }
 
